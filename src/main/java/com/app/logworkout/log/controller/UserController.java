4b9a13fcff19ae003293ec59060ec81eb07e69f1
Filipinzdr <saipaladoido@gmail.com>
2026-01-05 03:10:30 -0300

    Remove unused user endopoints

--- src/main/java/com/app/logworkout/log/controller/UserController.java
+++ src/main/java/com/app/logworkout/log/controller/UserController.java
@@ -22,23 +22,6 @@ public class UserController {
         this.serv = serv;
     }
 
-
-    @PostMapping
-    public UserResponseDTO create(@Valid @RequestBody UserCreateDTO dto){
-        return serv.create(dto);
-
-    }
-
-    @GetMapping("/{id}")
-    public UserResponseDTO findById(@PathVariable Long id){
-        return serv.findById(id);
-    }
-
-    @GetMapping
-    public List<UserResponseDTO> findAll(){
-        return serv.findAll();
-    }
-
     @GetMapping("/me")
     public UserResponseDTO me(@AuthenticationPrincipal User user){
         return serv.getMe(user);
